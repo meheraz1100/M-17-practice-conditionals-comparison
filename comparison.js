168bcb67414bc5acfb1f3f0a8755bfe553f454e8
meheraz1100 <meheraz1100@gmail.com>
2024-01-29 11:49:29 +0600

    recap the last module

---
+++ comparison.js
@@ -0,0 +1,9 @@
+/**
+ * COMPARISON
+ * 1. Bigger:  >
+ * 2. less:    <
+ * 3. equal:   ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal   : <=
+ * 6. 
+ */
---
+++ variable.js
@@ -0,0 +1,35 @@
+/*
+* vaiable ==> container
+
+*/
+
+//number type
+var sunGlasses = 80;
+
+//string
+var subject = TCT;
+
+//boolean
+var isPassed = true;
+
+/*
+* NAMING CONVENTION 
+* single word
+* no quote
+* no dash
+* cannot start with a number
+* cannot use reserve keyword
+* prefer camelcase
+*/
+
+var lastExamResult = 89;  //preferable
+
+
+/**
+ * Operation
+ * 1,+, -, *, /, %
+ * 2. += , -=, *=, /=, %=
+ * 3. isNaN
+ * 5. null
+ * 6. Number
+ */
